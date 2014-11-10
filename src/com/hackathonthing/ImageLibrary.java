a58ecc8170edb104c73dca9c2add1713adb016d5
jgzuke <jgzuke@gmail.com>
2014-11-10 16:14:41 -0500

    First commit, almost working layout
    Changing rows to be created from xml, fixing padding, spacing etc.

---
+++ src/com/hackathonthing/ImageLibrary.java
@@ -0,0 +1,78 @@
+/**
+ * Loads, stores and resizes all graphics
+ */
+package com.hackathonthing;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+public final class ImageLibrary
+{
+	private String getting;
+	protected Resources res;
+	protected String packageName;
+	protected BitmapFactory.Options opts;
+	protected BitmapDrawable [] notifOpts;
+	protected BitmapDrawable edit;
+	protected BitmapDrawable plus;
+	protected BitmapDrawable delete;
+	/**
+	 * loads in images and optimizes settings for loading
+	 * @param contextSet start activity for getting resources etc
+	 * @param controlSet control object
+	 */
+	public ImageLibrary(Context contextSet)
+	{
+		opts = new BitmapFactory.Options();
+		opts.inDither = false;
+		opts.inTempStorage = new byte[16 * 1024];
+		packageName = contextSet.getPackageName();
+		res = contextSet.getResources();
+		edit = loadImage("edit", 120, 120);
+		plus = loadImage("plus", 60, 60);
+		delete = loadImage("delete", 80, 80);
+		notifOpts = loadArray(3, "notif", 80, 80);
+	}
+	/**
+	 * Loads and resizes array of images
+	 * @param length Length of array to load
+	 * @param start Starting string which precedes array index to match resource name
+	 * @param width End width of image being loaded
+	 * @param height End height of image being loaded
+	 */
+	protected BitmapDrawable[] loadArray(int length, String start, int width, int height)
+	{
+		BitmapDrawable[] newArray = new BitmapDrawable[length];
+		for(int i = 0; i < length; i++)
+		{
+			getting = start + correctDigits(i + 1, 4);
+			newArray[i] = loadImage(getting, width, height);
+		}
+		return newArray;
+	}
+	/**
+	 * Adds 0's before string to make it four digits long
+	 * Animations done in flash which when exporting .png sequence end file name with four character number
+	 * @return Returns four character version of number
+	 */
+	protected String correctDigits(int start, int digits)
+	{
+		String end = Integer.toString(start);
+		while(end.length() < digits)
+		{
+			end = "0" + end;
+		}
+		return end;
+	}
+	/**
+	 * Loads image of name given from resources and scales to specified width and height
+	 * @return Returns bitmap loaded and resized
+	 */
+	protected BitmapDrawable loadImage(String imageName, int width, int height)
+	{
+		int imageNumber = res.getIdentifier(imageName, "drawable", packageName);
+		return new BitmapDrawable(res, Bitmap.createScaledBitmap(BitmapFactory.decodeResource(res, imageNumber, opts), width, height, false));
+	}
+}
---
+++ src/com/hackathonthing/MainActivity.java
@@ -0,0 +1,434 @@
+package com.hackathonthing;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	private MainActivity myself;
+	private LinearLayout mainLayout;
+	private byte numPresets = 3;
+	private String [] presets = {"Home", "Sleep", "Work", "", "", ""};
+	private Button presetButtons [] = new Button[6];
+	private Resources r;
+	private int iconSize = 25;
+	private double dpToPx;
+	private TableLayout presetsTable;
+	private TableRow presetButtonRow;
+	private ArrayList<ArrayList<int[][]>> times =  new ArrayList<ArrayList<int[][]>>(); // preset, rule, [start/end][day, hour, min]
+	private ArrayList<ArrayList<String[]>> rules = new ArrayList<ArrayList<String[]>>();
+	private TableLayout rulesTable;
+	private TableLayout timesTable;
+	private int current = 0;
+	private ImageLibrary imageLibrary;
+	private ImageButton editPresets;
+	private ImageButton editRules;
+	private ImageButton editTimes;
+	private TextView notificationRules;
+	private TextView activeTimes;
+	private TableRow.LayoutParams rowLayout;
+	private TableLayout.LayoutParams tableLayout;
+	private LayoutInflater layoutInflater;
+	private View editScreen;
+	private PopupWindow popup;
+	@Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        myself = this;
+        setContentView(R.layout.activity_main);
+        r = getResources();
+        layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);  
+        imageLibrary = new ImageLibrary(this);
+        dpToPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1000, r.getDisplayMetrics())/1000;
+        setStaticTablesButtons();
+        makePresetBase();
+        setUpDefaultPresets();
+        buildPresetButtons();
+        loadPreset(0);
+    }
+	private void setStaticTablesButtons()
+	{
+		mainLayout = (LinearLayout) findViewById( R.id.mainLayout);
+		notificationRules = (TextView)findViewById( R.id.notificationRules);
+        activeTimes = (TextView)findViewById( R.id.activeTimes);
+        notificationRules.setTextSize(20);
+        activeTimes.setTextSize(20);
+        editPresets = (ImageButton) findViewById( R.id.editPresets);
+    	editRules = (ImageButton) findViewById( R.id.editRules);
+    	editTimes = (ImageButton) findViewById( R.id.editTimes);
+        presetsTable = (TableLayout) findViewById( R.id.presetsTable);
+        rulesTable = (TableLayout) findViewById( R.id.rulesTable);
+        rulesTable.setStretchAllColumns(true);
+        rulesTable.setShrinkAllColumns(true);
+        timesTable = (TableLayout) findViewById( R.id.timesTable);
+        timesTable.setStretchAllColumns(true);
+        timesTable.setShrinkAllColumns(true);
+        editPresets.setImageDrawable(imageLibrary.edit);
+        editRules.setImageDrawable(imageLibrary.plus);
+        editTimes.setImageDrawable(imageLibrary.plus);
+        editPresets.setOnClickListener(editPresetsClickHandler);
+        editRules.setOnClickListener(editRulesClickHandler);
+        editTimes.setOnClickListener(editTimesClickHandler);
+	}
+	private void makePresetBase()
+	{
+		for(int i = 0; i < 3; i ++)
+		{
+			ArrayList<int[][]> t =  new ArrayList<int[][]>();
+			ArrayList<String[]> r = new ArrayList<String[]>();
+			times.add(t);
+			rules.add(r);
+		}
+	}
+	private void setUpDefaultPresets()
+	{
+        makeRule("Calls", "Default", "silent");
+        makeRule("Texts", "Default", "vibrate");
+        makeRule("Email", "Default", "ring");
+        makeTime(1, 2, 3, 1, 2, 4);
+        makeTime(1, 2, 3, 1, 2, 4);
+	}
+    private void buildPresetButtons()
+    {
+    	presetButtonRow = new TableRow(this);
+    	presetsTable.addView(presetButtonRow);
+        for(int i = 0; i < numPresets; i++)
+        {
+        	presetButtons[i] = new Button(this);
+        	presetButtons[i].setText(presets[i]);
+        	presetButtons[i].setTextSize(20);
+        	presetButtons[i].setWidth(pixels(100));
+        	presetButtons[i].setHeight(pixels(60));
+        	presetButtons[i].setId(i+10000);
+        	presetButtons[i].setOnClickListener(presetClickHandler);
+        	presetButtonRow.addView(presetButtons[i], i);
+        }
+    }
+    private void makeRule(String program, String person, String action)
+    {
+    	String[] newRule = new String[3];
+    	newRule[0]=program;
+    	newRule[1]=person;
+    	newRule[2]=action;
+    	rules.get(current).add(newRule);
+    }
+    private void makeTime(int startD, int startH, int startM, int endD, int endH, int endM)
+    {
+    	int[][] newTime = new int[2][3];
+    	newTime[0][0]=startD;
+    	newTime[0][1]=startH;
+    	newTime[0][2]=startM;
+    	newTime[1][0]=endD;
+    	newTime[1][1]=endH;
+    	newTime[1][2]=endM;
+    	times.get(current).add(newTime);
+    }
+    private void buildRuleRows()
+    {
+    	rulesTable.removeAllViews();
+    	for(int i = 0; i < rules.get(current).size(); i ++)
+    	{
+    		TableRow r = new TableRow(this);
+        	TextView program = new TextView(this);
+        	TextView person = new TextView(this);
+        	Button action = new Button(this);
+        	Button delete = new Button(this);
+        	String [] values = rules.get(current).get(i);
+        	program.setText(values[0]);
+        	program.setTextSize(20);
+        	program.setWidth(pixels(110));
+        	program.setId(21000+i);
+        	
+        	person.setText(values[1]);
+        	person.setTextSize(20);
+        	person.setWidth(pixels(110));
+        	person.setId(22000+i);
+        	
+        	action.setBackground(imageLibrary.notifOpts[actToID(values[2])]);
+        	action.setWidth(pixels(iconSize));
+        	action.setHeight(pixels(iconSize));
+        	action.setOnClickListener(notifClickHandler);
+        	action.setId(23000+i);
+        	
+        	delete.setBackground(imageLibrary.delete);
+        	delete.setWidth(pixels(iconSize));
+        	delete.setHeight(pixels(iconSize));
+        	delete.setOnClickListener(deleteRuleClickHandler);
+        	delete.setId(24000+i);
+
+        	r.addView(program);
+        	r.addView(person);
+        	r.addView(action);
+        	r.addView(delete);
+        	r.setId(i+20000);
+        	r.setBackgroundColor(Color.LTGRAY);
+        	rowLayout = new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        	rowLayout.setMargins(900, 50, 80, 160);
+        	r.setLayoutParams(rowLayout);
+        	rulesTable.addView(r, rowLayout);
+        	rulesTable.requestLayout();
+        }
+    }
+    private int actToID(String action)
+    {
+    	if(action.equals("silent")) return 0;
+    	if(action.equals("vibrate")) return 1;
+    	return 2;
+    }
+    private void buildTimeRows()
+    {
+    	timesTable.removeAllViews();
+    	for(int i = 0; i < times.get(current).size(); i ++)
+    	{
+    		TableRow t = new TableRow(this);
+    		TextView start = new TextView(this);
+        	TextView end = new TextView(this);
+        	Button delete = new Button(this);
+        	int [][] values = times.get(current).get(i);
+        	start.setText(timeToString(values[0])+ " to ");
+        	start.setTextSize(20);
+        	start.setWidth(pixels(140));
+        	start.setId(31000+i);
+        	
+        	end.setText(timeToString(values[1]));
+        	end.setTextSize(20);
+        	end.setWidth(pixels(140));
+        	end.setId(32000+i);
+        	
+        	delete.setBackground(imageLibrary.delete);
+        	delete.setWidth(pixels(iconSize));
+        	delete.setHeight(pixels(iconSize));
+        	delete.setGravity(Gravity.RIGHT);
+        	delete.setId(33000+i);
+        	delete.setOnClickListener(deleteTimeClickHandler);
+        	
+        	t.addView(start);
+        	t.addView(end);
+        	t.addView(delete);
+        	t.setId(i+30000);
+        	t.setBackgroundColor(Color.LTGRAY);
+        	
+        	rowLayout = new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        	rowLayout.topMargin=900;
+        	t.setLayoutParams(rowLayout);
+        	timesTable.addView(t, rowLayout);
+    	}
+    }
+    private void notifClick(int index)
+    {
+    	switch(actToID(rules.get(current).get(index)[2]))
+    	{
+    		case 0:
+    			rules.get(current).get(index)[2]="vibrate";
+    			break;
+    		case 1:
+    			rules.get(current).get(index)[2]="ring";
+    			break;
+    		case 2:
+    			rules.get(current).get(index)[2]="silent";
+    			break;
+    	}
+    	buildRuleRows();
+    }
+    private void deleteRule(int index)
+    {
+    	rules.get(current).remove(index);
+    	TableRow row;
+    	for(int i = index; i < rules.get(current).size(); i++)
+    	{
+    		row = (TableRow) rulesTable.getChildAt(index);
+    		int ID = row.getId()-20001;
+    		row.setId(20000+ID);
+    		row.getChildAt(0).setId(21000+ID);
+    		row.getChildAt(1).setId(22000+ID);
+    		row.getChildAt(2).setId(23000+ID);
+    		row.getChildAt(3).setId(24000+ID);
+    	}
+    	buildRuleRows();
+    }
+    private void deleteTime(int index)
+    {
+    	times.get(current).remove(index);
+    	TableRow row;
+    	for(int i = index; i < rules.get(current).size(); i++)
+    	{
+    		row = (TableRow) rulesTable.getChildAt(index);
+    		int ID = row.getId()-30001;
+    		row.setId(30000+ID);
+    		row.getChildAt(0).setId(31000+ID);
+    		row.getChildAt(1).setId(32000+ID);
+    		row.getChildAt(2).setId(33000+ID);
+    	}
+    	buildTimeRows();
+    }
+    private String timeToString(int [] time)
+    {
+    	return intToDay(time[0])+" "+Integer.toString(time[1])+":"+Integer.toString(time[2]);
+    }
+    private String intToDay(int day)
+    {
+    	switch(day)
+    	{
+    		case 1: return "Sun";
+    		case 2: return "Mon";
+    		case 3: return "Tue";
+    		case 4: return "Wed";
+    		case 5: return "Thu";
+    		case 6: return "Fri";
+    		case 7: return "Sat";
+    	}
+    	return "";
+    }
+    private void makePreset()
+    {
+    	numPresets++;
+    	Button b = new Button(this);
+        b.setText(presets[numPresets-1]);
+        b.setTextSize(20);
+        b.setId(numPresets-1+100);
+        b.setOnClickListener(presetClickHandler);
+        b.setHeight(pixels(60));
+        presetButtons[numPresets-1] = b;
+        for(int i = 0; i < numPresets; i++)
+        {
+        	presetButtons[i].setWidth(pixels(300/numPresets));
+        }
+        presetButtonRow.addView(b, numPresets-1);
+    }
+    private void removePreset(int toRemove)
+    {
+    	numPresets--;
+    	for(int i = toRemove; i < numPresets; i ++)
+    	{
+    		presetButtons[i]=presetButtons[i+1];
+    		presetButtonRow.removeViewAt(i);
+    		presetButtonRow.addView(presetButtons[i], i);
+    	}
+    	presetButtons[numPresets]=null;
+        presetButtonRow.removeViewAt(numPresets);
+    }
+    private void loadPreset(int preset)
+    {
+    	current = preset;
+    	
+    	buildRuleRows();
+    	buildTimeRows();
+    }
+    private int pixels(int dp)
+    {
+    	return (int)(dp*dpToPx);
+    }
+	View.OnClickListener presetClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(View v)
+	    {
+	    	loadPreset(v.getId()-10000);
+	    }
+	};
+	View.OnClickListener notifClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(View v)
+	    {
+	    	notifClick(v.getId()-23000);
+	    }
+	};
+	View.OnClickListener deleteRuleClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(final View v)
+	    {
+	        View popupView = layoutInflater.inflate(R.layout.popup_delete, null);  
+	        popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);  
+	        Button yes = (Button) popupView.findViewById(R.id.yes);
+	        Button no = (Button) popupView.findViewById(R.id.no);
+	        yes.setOnClickListener(new View.OnClickListener()
+	        {
+	            public void onClick(View b)
+	            {
+	            	deleteRule(v.getId()-24000);
+	            	popup.dismiss();
+	            }
+	        });
+	        no.setOnClickListener(new View.OnClickListener()
+	        {
+	            public void onClick(View v)
+	            {
+	            	popup.dismiss();
+	            }
+	        });
+	        popup.setContentView(popupView);
+	        popup.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+	    }
+	};
+	View.OnClickListener deleteTimeClickHandler = new View.OnClickListener()
+	{
+		public void onClick(final View v)
+	    {
+			View popupView = layoutInflater.inflate(R.layout.popup_delete, null);  
+	        popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);  
+	        Button yes = (Button) popupView.findViewById(R.id.yes);
+	        Button no = (Button) popupView.findViewById(R.id.no);
+	        yes.setOnClickListener(new View.OnClickListener()
+	        {
+	            public void onClick(View b)
+	            {
+	            	deleteRule(v.getId()-24000);
+	            	popup.dismiss();
+	            }
+	        });
+	        no.setOnClickListener(new View.OnClickListener()
+	        {
+	            public void onClick(View v)
+	            {
+	            	popup.dismiss();
+	            }
+	        });
+	        popup.setContentView(popupView);
+	        popup.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+	    }
+	};
+	View.OnClickListener editPresetsClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(View v)
+	    {
+	    	//editScreen = layoutInflater.inflate(R.layout.activity_presets, null);  
+	        //final PopupWindow popupWindow = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);  
+	        //TODO text box and plus for new
+	    	//TODO list of old 
+	    }
+	};
+	View.OnClickListener editRulesClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(View v)
+	    {
+	    	//editScreen = layoutInflater.inflate(R.layout.activity_rules, null);  
+	    	//TODO two text boxes, and three button, highlight one, add button
+	    }
+	};
+	View.OnClickListener editTimesClickHandler = new View.OnClickListener()
+	{
+	    public void onClick(View v)
+	    {
+	    	//editScreen = layoutInflater.inflate(R.layout.activity_times, null);  
+	    	//TODO two week scrolls and clocks Add button
+	    }
+	};
+}
